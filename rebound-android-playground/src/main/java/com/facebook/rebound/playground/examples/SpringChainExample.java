4422a11deb165b64ca7e729e464a7ce548c960b9
brucetoo <brucetoo14@gmail.com>
2016-06-07 15:28:28 +0800

    SpringChain Example....

--- rebound-android-playground/src/main/java/com/facebook/rebound/playground/examples/SpringChainExample.java
+++ rebound-android-playground/src/main/java/com/facebook/rebound/playground/examples/SpringChainExample.java
@@ -74,8 +74,10 @@ public class SpringChainExample extends FrameLayout {
           view.setTranslationX(value);
         }
       });
+      //color通过计算view count的Friction来获取 ArgbEvaluator 对应的值
       int color = (Integer) evaluator.evaluate((float) i / (float) viewCount, startColor, endColor);
       view.setBackgroundColor(color);
+      //每个view都设置touch事件
       view.setOnTouchListener(new OnTouchListener() {
         @Override
         public boolean onTouch(View v, MotionEvent event) {
@@ -91,6 +93,7 @@ public class SpringChainExample extends FrameLayout {
       public void onGlobalLayout() {
         getViewTreeObserver().removeOnGlobalLayoutListener(this);
         List<Spring> springs = mSpringChain.getAllSprings();
+        //其实以下的初始化设置值都可要可不要的,主要是初始化的时候设置SpringChain相应初始值
         for (int i = 0; i < springs.size(); i++) {
           springs.get(i).setCurrentValue(-mViews.get(i).getWidth());
         }
@@ -112,31 +115,43 @@ public class SpringChainExample extends FrameLayout {
     switch (action & MotionEvent.ACTION_MASK) {
       case MotionEvent.ACTION_DOWN:
 
+        //记录按下的点的id
         mActivePointerId = event.getPointerId(0);
+        //按下时 View 对应的translationX offset
         mLastDownXlat = view.getTranslationX();
+        //记录按下的被拖拽的view
         mLastDraggingView = view;
+        //按下点的在屏幕中的 x 坐标
         mLastDownX = event.getRawX();
 
+        //按下时 开始监听手指滑动的速率
         mVelocityTracker = VelocityTracker.obtain();
         mVelocityTracker.addMovement(event);
 
+        //通过view add的顺序获取draggingView对应index
         int idx = mViews.indexOf(mLastDraggingView);
+        //设置SpringChain对应的ControlSpringIndex为 index
         mSpringChain
             .setControlSpringIndex(idx)
             .getControlSpring()
-            .setCurrentValue(mLastDownXlat);
+            .setCurrentValue(mLastDownXlat);//view的位移设置为当前的translationX值
         break;
       case MotionEvent.ACTION_MOVE: {
+        //如果ACTION_DOWN记录的pointer id != -1
         final int pointerIndex = event.findPointerIndex(mActivePointerId);
         if (pointerIndex != -1) {
           final int location[] = {0, 0};
           view.getLocationOnScreen(location);
           float x = event.getX(pointerIndex) + location[0];
-          float offset = x - mLastDownX + mLastDownXlat;
+//          float offset = x - mLastDownX + mLastDownXlat;
+          //以上使用 getLocationOnScreen方法获取到移动的点在屏幕中的位置
+          //实际可用用 event.getRawX来获取,效果一样.但是如果要获取 Y坐标
+          //需要event.getRawY - statusBar的高度
+          float offset = event.getRawX() - mLastDownX + mLastDownXlat;
           mSpringChain
-              .getControlSpring()
+              .getControlSpring()//need get control spring first,and setCurrentValue
               .setCurrentValue(offset);
-          mVelocityTracker.addMovement(event);
+          mVelocityTracker.addMovement(event);//listen move velocity
         }
         break;
       }
